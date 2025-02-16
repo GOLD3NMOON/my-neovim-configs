a219bca4c0241afdc82e49bde760b5c5943d0284
GOLD3NMOON <gold3nmoon1@gmail.com>
2025-02-16 02:50:11 -0300

    (refactor) refactored transparent

---
+++ lua/core/transparent.lua
@@ -0,0 +1,14 @@
+local groups = {
+  "Normal", "NormalNC", "NormalFloat", "Comment", "Constant", "Special", "Identifier",
+  "Statement", "PreProc", "Type", "Underlined", "Todo", "String", "Function",
+  "Conditional", "Repeat", "Operator", "Structure", "LineNr", "NonText",
+  "SignColumn", "CursorLine", "CursorLineNr", "EndOfBuffer",
+  "Pmenu", "PmenuSel", "PmenuSbar", "PmenuThumb", "TabLineFill",
+  "StatusLine", "StatusLineNC"
+}
+
+for _, group in ipairs(groups) do
+  vim.api.nvim_set_hl(0, group, { bg = "none" })
+end
+
+vim.cmd("colorscheme moonfly")
--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -21,4 +21,13 @@ return {
 			"rcarriga/nvim-notify",
 		},
 	},
+    {
+        "rcarriga/nvim-notify",
+        config = function()
+            require("notify").setup({
+                background_colour = "#000000",        
+        })
+    end
+
+    }
 }
